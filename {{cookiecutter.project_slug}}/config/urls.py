7b9fad095c2bc9613721a45f65fbaa0b9f061481
Mazdak Badakhshan <geraneum@gmail.com>
2018-05-19 23:22:11 +0430

    Migrate to Django's new path module

--- {{cookiecutter.project_slug}}/config/urls.py
+++ {{cookiecutter.project_slug}}/config/urls.py
@@ -1,24 +1,29 @@
+import logging
+from collections import OrderedDict
+
+from django.urls import include
 from django.conf import settings
 from django.contrib import admin
-from django.conf.urls import include, url
 from django.conf.urls.static import static
+from django.urls import path, NoReverseMatch
 from django.views.generic import RedirectView
 from django.views import defaults as default_views
-from django.urls import path
 
-from rest_framework.reverse import reverse_lazy
+from rest_framework.reverse import reverse_lazy, reverse
 from rest_framework.routers import APIRootView
+
+logger = logging.getLogger(__name__)
 
 urlpatterns = [
     {% if cookiecutter.use_grappelli == "y" -%}
     # Django Grappelli
     path('grappelli/', include('grappelli.urls')),
     {%- endif %}
-    # Django Admin, use {% raw %}{% url 'admin:index' %}{% endraw %}
-    url(settings.ADMIN_URL, admin.site.urls),
+    # Django Admin, use {% url 'admin:index' %}
+    path(settings.ADMIN_URL, admin.site.urls),
 
     # API Root View Session Auth
-    url(r'^root-view-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('root-view-auth', include('rest_framework.urls', namespace='rest_framework')),
 
     # API V1
     url(r'^v1/', include(([
@@ -26,39 +31,38 @@ urlpatterns = [
         # TODO: custom urls includes go here
 
         # === root view
-        url(r'^$', APIRootView.as_view(), name='root'),
+        path('', APIRootView.as_view(), name='root'),
 
-    ], '{{cookiecutter.project_slug}}-api-v1'), namespace='v1')),
+    ], '{{cookiecutter.project_slug}}'), namespace='v1')),
 
     # Service Root View
-    url(r'^$', RedirectView.as_view(url=reverse_lazy('v1:root'), permanent=False)),
+    path('', RedirectView.as_view(url=reverse_lazy('v1:root'), permanent=False)),
 
 ] + static(
     settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
 )
 
 if settings.DEBUG:
-    # This allows the error pages to be debugged during development, just visit
-    # these url in browser to see how these error pages look like.
+    # This allows the error pages to be debugged during development.
     urlpatterns += [
-        url(
-            r"^400/$",
+        path(
+            '400/',
             default_views.bad_request,
             kwargs={"exception": Exception("Bad Request!")},
         ),
-        url(
-            r"^403/$",
+        path(
+            '403/',
             default_views.permission_denied,
             kwargs={"exception": Exception("Permission Denied")},
         ),
-        url(
-            r"^404/$",
+        path(
+            '404/',
             default_views.page_not_found,
             kwargs={"exception": Exception("Page not Found")},
         ),
-        url(r"^500/$", default_views.server_error),
+        path('500/', default_views.server_error),
     ]
     if "debug_toolbar" in settings.INSTALLED_APPS:
         import debug_toolbar
 
-        urlpatterns = [url(r"^__debug__/", include(debug_toolbar.urls))] + urlpatterns
+        urlpatterns = [path('__debug__/', include(debug_toolbar.urls))] + urlpatterns
