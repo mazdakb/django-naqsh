b519f71c9ed7e5e343aafe3104b8dc7ed276b7e2
Mazdak Badakhshan <geraneum@gmail.com>
2018-05-22 23:47:09 +0430

    Fix conditional socket module import in local settings

--- {{cookiecutter.project_slug}}/config/settings/local.py
+++ {{cookiecutter.project_slug}}/config/settings/local.py
@@ -65,6 +65,7 @@ INTERNAL_IPS = ['127.0.0.1', '10.0.2.2']
 
 {% if cookiecutter.use_docker == 'y' -%}
 if env('USE_DOCKER') == 'yes':
+    import socket  # noqa F405
     hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
     INTERNAL_IPS += [ip[:-1] + '1' for ip in ips]
 {%- endif %}
