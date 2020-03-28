b89dccd88e38f06fc6a00d87e8e462764286ec0b
Mazdak Badakhshan <geraneum@gmail.com>
2020-03-28 15:57:06 +0100

    Fix storages black template

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/common/storages.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/common/storages.py
@@ -1,10 +1,9 @@
+{% if cookiecutter.cloud_provider == 'AWS' -%}
 import os
 
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 
-
-{% if cookiecutter.cloud_provider == 'AWS' -%}
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
@@ -16,19 +15,6 @@ class StaticRootS3Boto3Storage(S3Boto3Storage):
 class MediaRootS3Boto3Storage(S3Boto3Storage):
     location = "media"
     file_overwrite = False
-{%- elif cookiecutter.cloud_provider == 'GCP' -%}
-from storages.backends.gcloud import GoogleCloudStorage
-
-
-class StaticRootGoogleCloudStorage(GoogleCloudStorage):
-    location = "static"
-    default_acl = "publicRead"
-
-
-class MediaRootGoogleCloudStorage(GoogleCloudStorage):
-    location = "media"
-    file_overwrite = False
-{%- endif %}
 
 
 class OverwriteStorage(FileSystemStorage):
@@ -45,3 +31,57 @@ class OverwriteStorage(FileSystemStorage):
         if self.exists(name):
             os.remove(os.path.join(settings.MEDIA_ROOT, name))
         return name
+{%- elif cookiecutter.cloud_provider == 'GCP' -%}
+import os
+
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+
+from storages.backends.gcloud import GoogleCloudStorage
+
+
+class StaticRootGoogleCloudStorage(GoogleCloudStorage):
+    location = "static"
+    default_acl = "publicRead"
+
+
+class MediaRootGoogleCloudStorage(GoogleCloudStorage):
+    location = "media"
+    file_overwrite = False
+
+
+class OverwriteStorage(FileSystemStorage):
+    def get_available_name(self, name, max_length=None):
+        """Returns a filename that's free on the target storage system, and
+        available for new content to be written to.
+
+        Found at http://djangosnippets.org/snippets/976/
+
+        Notes:
+            This file storage solves overwrite on upload problem.
+        """
+        # If the filename already exists, remove it as if it was a true file system
+        if self.exists(name):
+            os.remove(os.path.join(settings.MEDIA_ROOT, name))
+        return name
+{%- else -%}
+import os
+
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+
+
+class OverwriteStorage(FileSystemStorage):
+    def get_available_name(self, name, max_length=None):
+        """Returns a filename that's free on the target storage system, and
+        available for new content to be written to.
+
+        Found at http://djangosnippets.org/snippets/976/
+
+        Notes:
+            This file storage solves overwrite on upload problem.
+        """
+        # If the filename already exists, remove it as if it was a true file system
+        if self.exists(name):
+            os.remove(os.path.join(settings.MEDIA_ROOT, name))
+{%- endif %}
