035fe96b50081b4851b60e6412e7893d6cc40091
Mazdak Badakhshan <geraneum@gmail.com>
2020-03-28 08:10:50 +0100

    Update accounts migration

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/accounts/migrations/0001_initial.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/accounts/migrations/0001_initial.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('auth', '0011_update_proxy_permissions'),
+        ('auth', '0012_alter_user_first_name_max_length'),
     ]
 
     operations = [
