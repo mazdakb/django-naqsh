c42d4c857008d5d1f383d333e7aaa887ccdd50dc
Mazdak Badakhshan <geraneum@gmail.com>
2020-05-22 12:20:31 +0200

    Remove extra endif

--- {{cookiecutter.project_slug}}/config/settings/base.py
+++ {{cookiecutter.project_slug}}/config/settings/base.py
@@ -310,4 +310,4 @@ CORS_ORIGIN_ALLOW_ALL = True
 INSTALLED_APPS.insert(0, "grappelli")
 GRAPPELLI_ADMIN_TITLE = "{{cookiecutter.project_name}}"
 GRAPPELLI_CLEAN_INPUT_TYPES = False
-{% endif %}{% endif %}
+{% endif %}
