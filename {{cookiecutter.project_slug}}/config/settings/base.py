6667bd9d2537383ad73d5353c31525cedc42dfd1
Mazdak Badakhshan <geraneum@gmail.com>
2020-11-21 02:20:46 +0100

    Fix black formatting for docker test

--- {{cookiecutter.project_slug}}/config/settings/base.py
+++ {{cookiecutter.project_slug}}/config/settings/base.py
@@ -275,7 +275,7 @@ CELERY_TASK_SOFT_TIME_LIMIT = 60
 # http://docs.celeryproject.org/en/latest/userguide/configuration.html#beat-scheduler
 CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
 {%- endif %}
-{% if cookiecutter.use_compressor == 'y' -%}
+{%- if cookiecutter.use_compressor == 'y' %}
 # django-compressor
 # ------------------------------------------------------------------------------
 # https://django-compressor.readthedocs.io/en/latest/quickstart/#installation
@@ -296,7 +296,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 25,
 }
-{% if cookiecutter.use_cors_package == 'y' -%}
+{%- if cookiecutter.use_cors_package == 'y' -%}
 # CORS CONFIGURATION
 # ------------------------------------------------------------------------------
 # Access Headers
